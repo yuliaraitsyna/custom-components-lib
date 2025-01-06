d8d7c4b2ff0e4f24afbc7d28a6a749c1b3b0b674
yuliaraitsyna <yuliaraitsyna@gmail.com>
2025-01-06 17:08:56 +0300

    feat: add jest and RTL

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    verbose: true,
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+    collectCoverageFrom: ['**/*.{ts,tsx}','!**/node_modules/**'],
+    coverageThreshold: {
+      global: {
+        branches: 100,
+        functions: 100,
+        lines: 100,
+        statements: 100,
+      },
+    },
+};
