884243b3b1a0b03e38ccf09b61ff864e2f494b5a
yuliaraitsyna <yuliaraitsyna@gmail.com>
2025-01-11 17:39:49 +0300

    fix: switch checked state

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -1,5 +1,5 @@
-import type {InputHTMLAttributes} from 'react';
-import React from 'react';
+import type {ChangeEvent, InputHTMLAttributes} from 'react';
+import React, {useState} from 'react';
 
 import styles from './Switch.module.css';
 
@@ -8,14 +8,21 @@ interface SwitchProps extends InputHTMLAttributes<HTMLInputElement> {
 }
 
 const Switch: React.FC<SwitchProps> = ({label, ...props}) => {
+  const [isChecked, setIsChecked] = useState<boolean>(!!props.checked);
+
   const labelText = props.required ? `${label}*` : label;
 
   const labelClassName = [styles.label, props.disabled && styles.disabled].filter(Boolean).join(' ');
 
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    setIsChecked(prev => !prev);
+    props.onChange?.(event);
+  };
+
   return (
     <div className={styles.container}>
       <label className={styles.switch}>
-        <input type="checkbox" {...props}></input>
+        <input {...props} type="checkbox" onChange={e => handleChange(e)} checked={isChecked}></input>
         <span className={styles.slider}></span>
       </label>
       <label className={labelClassName}>{labelText}</label>
