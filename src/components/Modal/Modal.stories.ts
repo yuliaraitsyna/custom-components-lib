084840f7b2dfe1a8c9daa608cca8bf14661e8a7a
yuliaraitsyna <yuliaraitsyna@gmail.com>
2025-01-11 20:55:49 +0300

    feat: add modal to stories

---
+++ src/components/Modal/Modal.stories.ts
@@ -0,0 +1,20 @@
+import type {Meta, StoryObj} from '@storybook/react/*';
+import {Modal} from './Modal';
+import React from 'react';
+
+const meta: Meta<typeof Modal> = {
+  title: 'Components/Modal',
+  component: Modal,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Modal>;
+
+export const Primary: Story = {
+  args: {
+    open: true,
+    onClose: () => console.log('close'),
+    children: [React.createElement('h1', {}, 'Header'), React.createElement('p', {}, 'This is a modal window')],
+  },
+};
