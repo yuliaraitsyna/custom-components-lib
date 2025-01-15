9561b6e4536c1831bc2ce85d0b75448ba89bf30b
yuliaraitsyna <yuliaraitsyna@gmail.com>
2025-01-15 16:56:41 +0300

    fix: fix stories

--- src/components/Select/Select.stories.ts
+++ src/components/Select/Select.stories.ts
@@ -1,5 +1,5 @@
 import type {Meta, StoryObj} from '@storybook/react';
-import Select from './Select';
+import {Select} from './Select';
 import React from 'react';
 
 const meta: Meta<typeof Select> = {
--- src/components/TextField/TextField.stories.ts
+++ src/components/TextField/TextField.stories.ts
@@ -1,5 +1,5 @@
 import type {Meta, StoryObj} from '@storybook/react';
-import TextField from './TextField';
+import {TextField} from './TextField';
 
 const meta: Meta<typeof TextField> = {
   title: 'Components/TextField',
